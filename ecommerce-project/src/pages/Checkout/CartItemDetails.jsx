c58f99d38a562b7582f6517ff899ee8c24c2d878
ARYAN PRASHAR <aryanprasher2@gmail.com>
2026-05-01 10:32:46 +0530

    Update API URL for cart item operations

--- ecommerce-project/src/pages/Checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/Checkout/CartItemDetails.jsx
@@ -2,15 +2,16 @@ import axios from "axios";
 import { useState } from "react";
 import { formatMoney } from "../../utils/Money";
 export function CartItemDetails({ cartItem, loadCart }) {
+    const API_URL = import.meta.env.VITE_API_URL;
     const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
     const [quantity, setQuantity] = useState(cartItem.quantity);
     const deleteCartItem = async () => {
-        await axios.delete(`/api/cart-items/${cartItem.productId}`);
+        await axios.delete(`${API_URL}/api/cart-items/${cartItem.productId}`);
         await loadCart();
     };
     const updateQuantity = async () => {
         if (isUpdatingQuantity) {
-            await axios.put(`/api/cart-items/${cartItem.productId}`, {
+            await axios.put(`${API_URL}/api/cart-items/${cartItem.productId}`, {
                 quantity: Number(quantity),
             });
             await loadCart();
